b463aef3a11517a55e77bb359cfdeae2d7123c27
jhoseph <jhoseph@gmail.com>
2021-01-21 21:02:42 +0100

    create Radio Button component and create bloodtype radiobutton form

--- components/NewUserForm.js
+++ components/NewUserForm.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from "react";
-import {
-  Text,
-  View,
-  TouchableHighlight,
-  StyleSheet,
-  TextInput,
-} from "react-native";
+import { Text, View, TouchableHighlight, StyleSheet } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 import auth from "@react-native-firebase/auth";
-import DateTimePickerModal from "react-native-modal-datetime-picker";
 import globalStyles from "../styles";
 import StepsIndicator from "./NewUserStepsIndicator";
+
+// Forms
+import PersonalDataForm from "./newUserForms/PersonalDataForm";
+import MedicalDataForm from "./newUserForms/MedicalDataForm";
 
 const NewUserForm = ({ navigation }) => {
   const [currentScreen, setCurrentScreen] = useState(0);
@@ -78,13 +75,21 @@ const NewUserForm = ({ navigation }) => {
 
   let ScreenForm = <Text>Hola mundo</Text>;
 
-  if (currentScreen === 0)
+  if (currentScreen === 0) {
     ScreenForm = (
       <PersonalDataForm
         userData={userData}
         handleFormChange={handleFormChange}
       />
     );
+  } else if (currentScreen === 1) {
+    ScreenForm = (
+      <MedicalDataForm
+        userData={userData}
+        handleFormChange={handleFormChange}
+      />
+    );
+  }
 
   return (
     <LinearGradient
@@ -152,90 +157,3 @@ const styles = StyleSheet.create({
 });
 
 export default NewUserForm;
-
-const PersonalDataForm = ({ userData, handleFormChange }) => {
-  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-
-  const showDatePicker = () => {
-    setDatePickerVisibility(true);
-  };
-
-  const hideDatePicker = () => {
-    setDatePickerVisibility(false);
-  };
-
-  const handleConfirm = (date) => {
-    console.log(new Date());
-    handleFormChange(date, "bornDate", "user");
-    hideDatePicker();
-  };
-
-  return (
-    <View>
-      <View style={globalStyles.inputBox}>
-        <TextInput
-          style={globalStyles.inputField}
-          autoCapitalize="none"
-          placeholder="Ingrese sus nombres"
-          placeholderTextColor="#b8b8b8"
-          onChangeText={(names) => handleFormChange(names, "names", "user")}
-          value={userData.names}
-        />
-      </View>
-      <View style={globalStyles.inputBox}>
-        <TextInput
-          style={globalStyles.inputField}
-          autoCapitalize="none"
-          placeholder="Ingrese sus apellidos"
-          placeholderTextColor="#b8b8b8"
-          onChangeText={(lastNames) =>
-            handleFormChange(lastNames, "lastNames", "user")}
-          value={userData.lastNames}
-        />
-      </View>
-      <View style={globalStyles.inputBox}>
-        <TextInput
-          style={globalStyles.inputField}
-          autoCapitalize="none"
-          placeholder="Ingrese su documento de identidad"
-          placeholderTextColor="#b8b8b8"
-          onChangeText={(dni) => handleFormChange(dni, "dni", "user")}
-          value={userData.dni}
-        />
-      </View>
-      <View>
-        <TouchableHighlight
-          style={[globalStyles.button, globalStyles.darkButton]}
-          onPress={showDatePicker}
-        >
-          <Text style={globalStyles.buttonText}>Fecha de nacimiento</Text>
-        </TouchableHighlight>
-        <DateTimePickerModal
-          isVisible={isDatePickerVisible}
-          mode="date"
-          onConfirm={handleConfirm}
-          onCancel={hideDatePicker}
-        />
-        {userData.bornDate !== "" && (
-          <View
-            style={[
-              globalStyles.button,
-              globalStyles.lightButton,
-              styles.dateBox,
-            ]}
-          >
-            <Text
-              style={[
-                globalStyles.buttonText,
-                globalStyles.lightButtonText,
-                styles.dateText,
-              ]}
-            >
-              {userData.bornDate.toLocaleDateString()}
-            </Text>
-          </View>
-        )}
-      </View>
-    </View>
-  );
-};
---
+++ components/newUserForms/PersonalDataForm.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  TouchableHighlight,
+  StyleSheet,
+  TextInput,
+} from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import globalStyles from "../../styles";
+
+const PersonalDataForm = ({ userData, handleFormChange }) => {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    console.log(new Date());
+    handleFormChange(date, "bornDate", "user");
+    hideDatePicker();
+  };
+
+  return (
+    <View>
+      <View style={globalStyles.inputBox}>
+        <TextInput
+          style={globalStyles.inputField}
+          autoCapitalize="none"
+          placeholder="Ingrese sus nombres"
+          placeholderTextColor="#b8b8b8"
+          onChangeText={(names) => handleFormChange(names, "names", "user")}
+          value={userData.names}
+        />
+      </View>
+      <View style={globalStyles.inputBox}>
+        <TextInput
+          style={globalStyles.inputField}
+          autoCapitalize="none"
+          placeholder="Ingrese sus apellidos"
+          placeholderTextColor="#b8b8b8"
+          onChangeText={(lastNames) =>
+            handleFormChange(lastNames, "lastNames", "user")
+          }
+          value={userData.lastNames}
+        />
+      </View>
+      <View style={globalStyles.inputBox}>
+        <TextInput
+          style={globalStyles.inputField}
+          autoCapitalize="none"
+          placeholder="Ingrese su documento de identidad"
+          placeholderTextColor="#b8b8b8"
+          onChangeText={(dni) => handleFormChange(dni, "dni", "user")}
+          value={userData.dni}
+        />
+      </View>
+      <View>
+        <TouchableHighlight
+          style={[globalStyles.button, globalStyles.darkButton]}
+          onPress={showDatePicker}
+        >
+          <Text style={globalStyles.buttonText}>Fecha de nacimiento</Text>
+        </TouchableHighlight>
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
+        />
+        {userData.bornDate !== "" && (
+          <View
+            style={[
+              globalStyles.button,
+              globalStyles.lightButton,
+              styles.dateBox,
+            ]}
+          >
+            <Text
+              style={[
+                globalStyles.buttonText,
+                globalStyles.lightButtonText,
+                styles.dateText,
+              ]}
+            >
+              {userData.bornDate.toLocaleDateString()}
+            </Text>
+          </View>
+        )}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  dateText: {
+    fontSize: 18,
+  },
+  dateBox: {
+    backgroundColor: "rgba(255,255,255,.6)",
+  },
+});
+
+export default PersonalDataForm;
