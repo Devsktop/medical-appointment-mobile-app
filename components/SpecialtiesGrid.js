9b50ef7511a77b998dea57ace1e171f583b492fd
jhoseph <jhoseph@gmail.com>
2021-02-18 15:41:44 +0100

    create specialtyBox to separate logic of each box from specialtiesGrid

--- components/SpecialtiesGrid.js
+++ components/SpecialtiesGrid.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import {
-  Text,
-  View,
-  StyleSheet,
-  ImageBackground,
-  ScrollView,
-} from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
+
+// Components
+import SpecialtyBox from "./SpecialtyBox";
 
 const specialtiesSelector = (state) => {
   const { specialtiesFilter, specialties } = state.doctors;
@@ -28,20 +25,7 @@ const Specialties = () => {
     <ScrollView>
       <View style={styles.container}>
         {Object.keys(specialties).map((key) => (
-          <View key={key} style={[styles.specialty]}>
-            <ImageBackground
-              source={{ uri: specialties[key].imageUrl }}
-              style={{
-                width: "100%",
-                height: 130,
-                justifyContent: "flex-end",
-              }}
-            >
-              <Text style={styles.specialtyText}>
-                {specialties[key].specialty}
-              </Text>
-            </ImageBackground>
-          </View>
+          <SpecialtyBox key={key} specialties={specialties[key]} id={key} />
         ))}
       </View>
     </ScrollView>
@@ -55,24 +39,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     flexWrap: "wrap",
-  },
-  specialty: {
-    width: "48%",
-    height: 120,
-    marginVertical: 10,
-    borderRadius: 5,
-    overflow: "hidden",
-  },
-  specialtyText: {
-    paddingVertical: 5,
-    paddingBottom: 10,
-    paddingLeft: 10,
-    backgroundColor: "rgba(0,0,0,.5)",
-    marginRight: 20,
-    marginBottom: 3,
-    borderTopRightRadius: 10,
-    color: "#fff",
-    fontWeight: "bold",
   },
 });
 
